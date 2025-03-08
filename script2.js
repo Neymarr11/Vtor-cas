3afcd9b2b5bd7793bda3dc54a62f83b4be332476
Neymarr11 <aaabbb6036541@gmail.com>
2025-03-08 14:53:03 +0100

    Update script2.js

--- script2.js
+++ script2.js
@@ -1,1 +1,20 @@
+textEl=document.getElementById('text')
+speedEl=document.getElementById('speed')
 
+text='Sakam da programiram'
+textPosition=1
+speed=500/speed.value
+
+writeText()
+
+function writeText(){
+    textEl.innerText=text.slice(0, textPosition)
+    textPosition++
+    if(textPosition>text.length){
+        textPosition=1
+    }
+    setTimeout(writeText, speed)
+    speedEl.addEventListener('input',(e) => {
+        speed = 500 / e.target.value
+    })
+}
